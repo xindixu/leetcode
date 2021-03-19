7db1d8a045b1fc59dfcbc5e264481b1ad6025f32
Xindi Xu <xindixu@utexas.edu>
2021-03-18 22:20:23 -0500

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-plusplus': 'off',
+    semi: ['error', 'never'],
+    'no-param-reassign': 'off',
+  },
+}
