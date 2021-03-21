98568261346b965a1672c090ebfc11a565e821de
Xindi Xu <xindixu@utexas.edu>
2021-03-21 17:07:10 -0500

    Q 64: review

--- 64.minimum-path-sum.js
+++ 64.minimum-path-sum.js
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+
 // Dynamic programming, reuse grid matrix
 // Time: O(m*n)
 // Space: O(1)
@@ -13,6 +14,37 @@
  * @return {number}
  */
 const minPathSum = (grid) => {
+  const rows = grid.length
+  const cols = grid[0].length
+
+  // initialize first row
+  for (let c = 1; c < cols; c++) {
+    grid[0][c] += grid[0][c - 1]
+  }
+
+  // initialize first col
+  for (let r = 1; r < rows; r++) {
+    grid[r][0] += grid[r - 1][0]
+  }
+
+  for (let r = 1; r < rows; r++) {
+    for (let c = 1; c < cols; c++) {
+      grid[r][c] += Math.min(grid[r - 1][c], grid[r][c - 1])
+    }
+  }
+
+  return grid[rows - 1][cols - 1]
+}
+// @lc code=end
+
+// Dynamic programming, reuse grid matrix
+// Time: O(m*n)
+// Space: O(1)
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum2 = (grid) => {
   const rows = grid.length
   const cols = grid[0].length
 
@@ -35,4 +67,3 @@ const minPathSum = (grid) => {
 
   return grid[rows - 1][cols - 1]
 }
-// @lc code=end
