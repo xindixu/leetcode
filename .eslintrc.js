e9b340ed54c75fc4a0b7cf91d688f5a91b8ea894
Xindi Xu <xindixu@utexas.edu>
2021-03-21 16:31:52 -0500

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  plugins: ["prettier"],
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
-    'no-plusplus': 'off',
-    semi: ['error', 'never'],
-    'no-param-reassign': 'off',
-    'no-continue': 'off',
-    'max-len': ['error', { ignoreComments: true }],
-    'no-restricted-syntax': 'off',
+    "no-plusplus": "off",
+    semi: ["error", "never"],
+    "no-param-reassign": "off",
+    "no-continue": "off",
+    "no-restricted-syntax": "off",
   },
 }
