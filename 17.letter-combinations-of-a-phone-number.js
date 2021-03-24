150700ba921abd1b6a45420b8834f97a7c149c5e
Xindi Xu <xindixu@utexas.edu>
2021-03-24 18:58:46 -0500

    Q 17: review

--- 17.letter-combinations-of-a-phone-number.js
+++ 17.letter-combinations-of-a-phone-number.js
@@ -5,12 +5,53 @@
  */
 
 // @lc code=start
+
+const MAP = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+}
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = (digits) => {
+  if (digits === "") {
+    return []
+  }
+
+  const results = []
+
+  const backtrack = (local, start) => {
+    if (local.length === digits.length) {
+      results.push(local)
+      return
+    }
+
+    const possibleChars = MAP[digits.charAt(start)]
+    for (const char of possibleChars) {
+      backtrack(`${local}${char}`, start + 1)
+    }
+  }
+
+  backtrack("", 0)
+  return results
+}
+
+// @lc code=end
+
 // backtrack
 // Time: O(4^n * n)
 // 4^n since max num of letters for each digit is 4. A tree with n levels and each level has max 4 branches
 // For each combination, it cost n to build it (since n digit, each digit is one letter)
 // Space: O(n)
-const MAP = {
+const MAP_ARR = {
   2: ["a", "b", "c"],
   3: ["d", "e", "f"],
   4: ["g", "h", "i"],
@@ -20,11 +61,12 @@ const MAP = {
   8: ["t", "u", "v"],
   9: ["w", "x", "y", "z"],
 }
+
 /**
  * @param {string} digits
  * @return {string[]}
  */
-const letterCombinations = (digits) => {
+const letterCombinations2 = (digits) => {
   const { length } = digits
   if (length === 0) {
     return []
@@ -32,7 +74,7 @@ const letterCombinations = (digits) => {
   const result = []
 
   const backtrack = (local, index) => {
-    const possibles = MAP[digits[index]]
+    const possibles = MAP_ARR[digits[index]]
     if (index >= length) {
       result.push(local.join(""))
       return
@@ -51,4 +93,3 @@ const letterCombinations = (digits) => {
   backtrack([], 0)
   return result
 }
-// @lc code=end
