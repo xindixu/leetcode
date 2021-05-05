98414ea79810a52f43d1515f68558da6dbc83931
Xindi Xu <xindixu@utexas.edu>
2021-05-04 22:38:40 -0500

    Q 344

---
+++ 344.reverse-string.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s) => {
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    ;[s[left], s[right]] = [s[right], s[left]]
+    left++
+    right--
+  }
+}
+// @lc code=end
