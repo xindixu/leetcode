45653fb328a9faa0fe999fe517d165f9a536a351
Xindi Xu <xindixu0@gmail.com>
2021-07-07 20:15:52 -0500

    Q 206 iteratively and recursively

--- 206.reverse-linked-list.js
+++ 206.reverse-linked-list.js
@@ -12,11 +12,40 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+// Recursively
+const reverseHelper = (head) => {
+  // if this is the original last node (new head)
+  if (head.next == null) {
+    return head
+  }
+
+  // 1 -> [ 2 <- 3 <- 4 <- 5 ]
+  //       next           last
+  const last = reverseHelper(head.next)
+  const { next } = head
+  //  1  <-  2
+  // head  next
+  next.next = head
+  head.next = null
+
+  // return the original last node (new head)
+  return last
+}
+
+const reverseList = (head) => {
+  if (!head) {
+    return null
+  }
+  return reverseHelper(head)
+}
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-const reverseList = (head) => {
+// Iteratively
+const reverseList2 = (head) => {
   let prev = null
   let cur = head
   while (cur) {
