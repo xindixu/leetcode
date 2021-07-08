f297eb6af327bc2cd076b8bd0611aad30360e8c6
Xindi Xu <xindixu0@gmail.com>
2021-07-07 19:58:56 -0500

    Q 206

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = (head) => {
+  let prev = null
+  let cur = head
+  while (cur) {
+    const { next } = cur
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+  return prev
+}
+// @lc code=end
