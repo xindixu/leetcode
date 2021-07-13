dc746fa631a06c1e4e5f6d1a031ea1090bf971db
Xindi Xu <xindixu0@gmail.com>
2021-07-12 21:00:38 -0500

    Q 219

--- 219.contains-duplicate-ii.js
+++ 219.contains-duplicate-ii.js
@@ -26,3 +26,38 @@ const containsNearbyDuplicate = (nums, k) => {
   return result
 }
 // @lc code=end
+
+const containsNearbyDuplicate2 = (nums, k) => {
+  const hash = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    if (hash[num] != undefined && i - hash[num] <= k) {
+      return true
+    }
+    hash[num] = i
+  }
+  return false
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+// sort by number then compare number and the original index
+const containsNearbyDuplicate2 = (nums, k) => {
+  const array = nums.map((num, index) => ({ num, index }))
+
+  array.sort((a, b) => a.num - b.num)
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (
+      array[i].num === array[i + 1].num &&
+      array[i + 1].index - array[i].index <= k
+    ) {
+      return true
+    }
+  }
+  return false
+}
